e8eaa434621241a315ba449f8e9def51e7f52c7f
raphaelw <raphaelw@users.noreply.github.com>
2023-12-03 20:39:59 +0100

    finish most basic tests

--- tests/test_emqf.py
+++ tests/test_emqf.py
@@ -5,6 +5,13 @@ import unittest
 import numpy as np
 import emqf
 
+
+def _complex_sort_keyfunc(complex_num):
+    """Sort by real part then by imag part"""
+    c = complex(complex_num)
+    return c.real, c.imag
+
+
 class TestSelectivityFactor(unittest.TestCase):
     TEST_SET = [
         {"kwargs": {"N": 2, "As": 25}, "verified_result": 8.90548901417296},
@@ -19,11 +26,12 @@ class TestSelectivityFactor(unittest.TestCase):
             kwargs = t["kwargs"]
             verified_result = t["verified_result"]
             xi = emqf.emqf_selectivity_factor(**kwargs)
-            self.assertAlmostEqual(xi, verified_result, 5)
+            self.assertAlmostEqual(xi, verified_result, places=5)
 
 
 class TestAnalogLowpass(unittest.TestCase):
     # TODO: add even order test data
+    # TODO: add data for gain (=k)
     TEST_SET = [
         {
             "kwargs": dict(N=7, xi=2.20227264783, f3db=True),
@@ -97,6 +105,18 @@ class TestAnalogLowpass(unittest.TestCase):
             self.assertTupleEqual(z_.shape, (len(z),))
             self.assertTupleEqual(p_.shape, (len(p),))
 
+    def _almost_equal_complex_unsorted_lists(self, list_a, list_b, places=5):
+        list_a = list( map(complex, list_a) )
+        list_b = list( map(complex, list_b) )
+        
+        list_a = sorted(list_a, key=_complex_sort_keyfunc)
+        list_b = sorted(list_b, key=_complex_sort_keyfunc)
+
+        self.assertEqual(len(list_a), len(list_b))
+
+        for a,b in zip(list_a, list_b):
+            self.assertAlmostEqual(a,b,places=places)
+
     def test_input_output_type(self):
         for t in __class__.TEST_SET:
             kwargs = t["kwargs"]
@@ -106,9 +126,11 @@ class TestAnalogLowpass(unittest.TestCase):
 
             z_, p_, k_ = emqf.emqf_analog_lowpass(**kwargs)
 
-            # shape
-            # self.assertEqual(len())
-            
+            self._almost_equal_complex_unsorted_lists(z, z_, places=5)
+            self._almost_equal_complex_unsorted_lists(p, p_, places=5)
+
+            # TODO: test for gain (=k)
+
 
 
 if __name__ == "__main__":
