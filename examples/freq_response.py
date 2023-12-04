06a996fc58f4c8b9f21ada85cfddd8929aa1563c
raphaelw <raphaelw@users.noreply.github.com>
2023-12-04 21:59:27 +0100

    rename package imports

--- examples/freq_response.py
+++ examples/freq_response.py
@@ -4,20 +4,20 @@ from scipy import signal
 import matplotlib.pyplot as plt
 import numpy as np
 
-import emqf
+import filterdesign
 
 N = 5
 As = 50
-xi = emqf.emqf_selectivity_factor(N, As)
-z, p, k = emqf.emqf_analog_lowpass(N=N, xi=xi, f3db=True)
+xi = filterdesign.emqf_selectivity_factor(N, As)
+z, p, k = filterdesign.emqf_analog_lowpass(N=N, xi=xi, f3db=True)
 
-b, a = signal.zpk2tf(z,p,k)
+b, a = signal.zpk2tf(z, p, k)
 w, h = signal.freqs(b, a, 2000)
 plt.semilogx(w, 20 * np.log10(abs(h)))
-plt.title('Filter frequency response')
-plt.xlabel('Frequency [radians / second]')
-plt.ylabel('Amplitude [dB]')
+plt.title("Filter frequency response")
+plt.xlabel("Frequency [radians / second]")
+plt.ylabel("Amplitude [dB]")
 plt.margins(0, 0.1)
-plt.grid(which='both', axis='both')
+plt.grid(which="both", axis="both")
 # plt.axvline(100, color='green') # cutoff frequency
 plt.show()
